4107f6e7ed29bb3b2d3cebefd4f1939dcf0fa218
Brian <amberbriansummer@comcast.net>
2016-11-04 17:38:10 -0600

    fix version number

--- gui.py
+++ gui.py
@@ -23,7 +23,7 @@ URLError=fnku.URLError
 HTTPError=fnku.HTTPError
 PhotoImage=tk.PhotoImage
 
-__VERSION__="2.0.4"
+__VERSION__="2.0.5"
 targetversion="FunKiiU v2.2"
 
 ### I'm getting about 80 titles not parsing from titlekeys.json
